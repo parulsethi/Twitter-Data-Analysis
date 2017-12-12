bf1dc0b208e48f632ab64e06440730de01abb541
parulsethi <parul1sethi@gmail.com>
2017-12-12 15:31:45 +0530

    add to mongodb

--- extract_tweets.py
+++ extract_tweets.py
@@ -1,7 +1,10 @@
 
+import json
+import re
 import tweepy
 import json
 import time
+from pymongo import MongoClient
 
 #Twitter API credentials
 consumer_key = 'my-consumer-key'
@@ -22,6 +25,13 @@ users = tweepy.Cursor(api.search, q=mumbai_rain_keywords, lang="en", include_ent
 count = 0
 errorCount=0
 
+client = MongoClient()
+
+delhi_db = client['delhi_db']
+delhi_collection = db['delhi_collection']
+mumbai_db = client['mumbai_db']
+mumbai_collection = db['mumbai_collection']
+
 while True:
     try:
         user = next(users)
@@ -35,9 +45,7 @@ while True:
     except StopIteration:
         break
     try:
-        with open('mumbai.json', 'a') as f:
-                json.dump(user._json, f)
-                f.write("\n")
+        mumbai_collection.insert(user._json)
         
     except UnicodeEncodeError:
         errorCount += 1
